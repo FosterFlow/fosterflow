10036588ce549dbbf6a6721cba1bd4555ced7bde
egortaran <egortaran@mail.ru>
2023-07-30 22:52:08 +0600

    #33 Answer from NLP saved in Database

--- backend/chat_app/consumers.py
+++ backend/chat_app/consumers.py
@@ -8,7 +8,7 @@ from channels.generic.websocket import WebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
 from project.settings import BASE_DIR
 from django.db.models import Q
-from chat_app.models import Chat
+from chat_app.models import Chat, Message
 from user_app.models import Agent
 
 env = environ.Env()
@@ -20,22 +20,28 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["chat_id"]
         self.chat_id = int(self.room_name)
-
-        agents = Agent.objects.filter(user_id=self.scope["user"])
-        available_chats = list(
-            Chat.objects.filter(Q(owner_id__in=agents) | Q(addressee_id__in=agents))
-            .values_list('id', flat=True))
-
-        if self.chat_id not in available_chats:
-            self.close()
+        self.chat = Chat.objects.get(id=self.chat_id)
 
         if self.scope["user"] == AnonymousUser():
             self.close()
+            return False
+
+        self.agents = Agent.objects.filter(user_id=self.scope["user"])
+        available_chats = list(
+            Chat.objects.filter(Q(owner_id__in=self.agents) | Q(addressee_id__in=self.agents))
+            .values_list('id', flat=True)
+        )
+
+        if self.chat_id not in available_chats:
+            self.close()
+            return False
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_name, self.channel_name
         )
+        self.owner_chat = Agent.objects.get(id=self.chat.owner_id.id)
+        self.addressee = Agent.objects.get(id=self.chat.addressee_id.id)
 
         self.accept()
 
@@ -52,7 +58,12 @@ class ChatConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({"message": prompt}))
 
         if prompt:
-            self.ask_gpt_stream(prompt)
+            complete = self.ask_gpt_stream(prompt)
+
+            # TODO Change owner_id correctly
+            Message.objects.create(chat_id=self.chat,
+                                   message_text=complete,
+                                   owner_id=self.owner_chat)
 
     # Receive message from room group
     def ask_gpt_stream(self, prompt):
@@ -68,13 +79,17 @@ class ChatConsumer(WebsocketConsumer):
             stream=True
         )
 
+        complete = ""
         # iterate through the stream of events
         for event in response:
             try:
                 event_text = event['choices'][0]['delta']['content']  # extract the text
                 self.send(text_data=json.dumps({"message": event_text}))
+                complete += event_text
             except:
                 pass
+
+        return complete
 
     # Receive message from room group
     def chat_message(self, event):
