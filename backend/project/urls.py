4037a1f043b654889ad76ba47502503d491f3638
egortaran <egortaran@mail.ru>
2023-08-13 21:44:02 +0600

    #81 Connect to websocket on ws/chats/(?P<chat_id>\w+)/$

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from django.urls import re_path
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_yasg.views import get_schema_view  # new
-from drf_yasg import openapi  # new
 from auth_app.views import UserLogoutAPIView, UserLoginAPIView, RegisterApi, \
     ConfirmEmailGenericAPIView, SendEmailConfirmationTokenAPIView, \
     ChangePasswordView
@@ -26,19 +20,6 @@ router.register('messages', MessageModelViewSet, basename='messages')
 router.register('users', UserModelViewSet, basename='users')
 router.register('agents', UserAgentModelViewSet, basename='agents')
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    # permission_classes=[permissions.AllowAny],
-)
-
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
@@ -46,7 +27,6 @@ urlpatterns = [
 
     path("chat/", index, name="index"),
     path("chat/<str:room_name>/", room, name="index"),
-
 
     path('api/token/', UserLoginAPIView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
@@ -62,7 +42,4 @@ urlpatterns = [
     path('api/confirmation-email/send/', SendEmailConfirmationTokenAPIView.as_view(),
          name='send_email_confirmation_api_view'),
     path('api/confirmation-email/confirm/', ConfirmEmailGenericAPIView.as_view(), name='account_confirm_email'),
-
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
