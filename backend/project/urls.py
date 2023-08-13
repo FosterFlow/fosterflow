341b6e689ab8b3600cc47e5af3ec86a3d86ccb45
egortaran <egortaran@mail.ru>
2023-08-13 17:31:55 +0600

    #79 url media

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -65,4 +65,4 @@ urlpatterns = [
 
     # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     # re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
