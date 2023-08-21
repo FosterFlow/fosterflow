9da2c4e0d921ef5ccb283a8de8b606aa5e637794
egortaran <egortaran@mail.ru>
2023-08-21 23:01:54 +0600

    #95 Send 403 error

--- backend/chat_app/middleware.py
+++ backend/chat_app/middleware.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AnonymousUser
 from jwt import decode as jwt_decode
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.response import Response
 
 Users = get_user_model()
 
@@ -23,8 +25,11 @@ class TokenAuthMiddleWare:
         self.app = app
 
     async def __call__(self, scope, receive, send):
-        headers = dict(scope['headers'])
-        token = headers[b'access'].decode("utf8").split()[0]
-        user = await get_user(token)
-        scope["user"] = user
-        return await self.app(scope, receive, send)
+        try:
+            headers = dict(scope['headers'])
+            token = headers[b'access'].decode("utf8").split()[0]
+            user = await get_user(token)
+            scope["user"] = user
+            return await self.app(scope, receive, send)
+        except Exception as e:
+            return await self.app(scope, receive, send)
