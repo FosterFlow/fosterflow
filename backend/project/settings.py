fb30b70497d37aadccbaf9e9175c912b8752d96b
egortaran <egortaran@mail.ru>
2023-08-29 17:25:40 +0600

    #110 Add password to Redis

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -22,12 +22,13 @@ ASGI_APPLICATION = "project.asgi.application"
 
 REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', 'password')
 
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(REDIS_HOST, REDIS_PORT)],
+            "hosts": [(f"redis://:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}/0")],
         },
     },
 }
