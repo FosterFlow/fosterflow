31f36cc4667eda7f04d569d5db9f1b7f19120350
egortaran <egortaran@mail.ru>
2023-08-30 18:02:41 +0600

    #125 Add EMAIL_TOKEN_LIFETIME_HOURS to env

--- backend/auth_app/models.py
+++ backend/auth_app/models.py
@@ -1,6 +1,10 @@
+import os
 from django.db import models
 from django.utils import timezone
 from user_app.models import User
+
+
+EMAIL_TOKEN_LIFETIME_HOURS = os.environ.get('EMAIL_TOKEN_LIFETIME_HOURS', 24)
 
 
 class EmailConfirmationToken(models.Model):
@@ -21,7 +25,7 @@ class EmailConfirmationToken(models.Model):
         verbose_name_plural = "Email Confirm Tokens"
 
     created_at = models.DateTimeField(auto_now_add=True)
-    expires_at = models.DateTimeField(default=(timezone.now() + timezone.timedelta(hours=2)))
+    expires_at = models.DateTimeField(default=(timezone.now() + timezone.timedelta(hours=EMAIL_TOKEN_LIFETIME_HOURS)))
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     key = models.CharField(
         max_length=64,
