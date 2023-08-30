c8a80a7586aedf4ba7c5ecafeda0abcd7756e6ad
egortaran <egortaran@mail.ru>
2023-08-30 19:24:29 +0600

    #126 Add 401 Error

--- backend/project/utils.py
+++ backend/project/utils.py
@@ -10,11 +10,15 @@ def custom_exception_handler(exc, context):
         }
 
     if response is not None and response.status_code == 401:
-        response.data = {
-            "errors": {
-                key: [value] for key, value in response.data.items()
+        for key, value in response.data.items():
+            if 'message' in value[0]:
+                value = 'Token is invalid or expired'
+            response.data = {
+                "errors": {
+                    'unauthorized': value
+                }
             }
-        }
+        print(response.data)
 
     if response is not None and response.status_code == 404:
         response.data = {
