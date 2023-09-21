d1048b7fb0ece25d75d12acff098bf26b551e3a2
egortaran <egortaran@mail.ru>
2023-09-21 19:48:21 +0300

    #173 Add test for answer into OpenAI

---
+++ backend/chat_app/tasks.py
@@ -0,0 +1,51 @@
+from celery import shared_task
+from time import sleep
+from .models import Chat, Message
+import openai
+
+
+@shared_task()
+def send_feedback_nlp_task(message_id, message_text, chat_id, owner_id):
+    chat_id = Chat.objects.get(id=chat_id)
+
+    nlp_message = Message.objects.create(
+        chat_id=chat_id,
+        message_text='',
+        owner_id=chat_id.addressee_id
+    )
+
+    previous_messages = Message.objects.filter(chat_id=chat_id).order_by('-id')[:5]
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+    main_message = Message.objects.get(id=message_id)
+
+    for message in previous_messages:
+        if message.owner_id != main_message.owner_id:
+            mes = {"role": "user", "content": message.message_text}
+        else:
+            mes = {"role": "assistant", "content": message.message_text}
+        messages.append(mes)
+
+    user_prompt = {"role": "user", "content": message_text}
+    messages.append(user_prompt)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        stream=True
+    )
+
+    complete = ""
+    for event in response:
+        try:
+            if event['choices'][0]['finish_reason'] != 'stop':
+                event_text = event['choices'][0]['delta']['content']
+                print(event_text)
+                complete += event_text
+                # todo To websocket
+        except Exception as e:
+            print('None')
+
+    nlp_message.message_text = complete
+    nlp_message.save()
