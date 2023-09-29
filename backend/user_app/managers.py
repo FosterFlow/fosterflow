83858c4e778d16dc583ad50983509b3ee1095ccc
egortaran <egortaran@mail.ru>
2023-09-29 14:18:15 +0300

    #189 Change View and Serializer for ProfileUser

--- backend/user_app/managers.py
+++ backend/user_app/managers.py
@@ -27,6 +27,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_email_confirmed', True)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError(_('Superuser must have is_staff=True.'))
