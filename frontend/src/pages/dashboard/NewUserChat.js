650698a26ba5bb67d1f3f8c5b4bbedc05dd67a7c
Nikita <bragin_na@soshace.com>
2023-10-05 21:01:46 +0300

    #203 Agents. Added selector into new chat for choosing an agent for the chat

--- frontend/src/pages/dashboard/NewUserChat.js
+++ frontend/src/pages/dashboard/NewUserChat.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { connect } from "react-redux";
 import withRouter from "../../components/withRouter";
 import ChatInput from "./UserChat/ChatInput";
 import UserHead from "./UserChat/UserHead";
 import { useTranslation } from 'react-i18next';
+import {
+    getAgents, 
+    setActiveAgent,
+} from "../../redux/actions";
 
 function UserChat(props) {
     const { t } = useTranslation();
+    const {
+        agents,
+        getAgents,
+        authorizedUser
+    } = props;
+
+    useEffect(() => {
+        if (authorizedUser === null ||
+            authorizedUser.is_email_confirmed === false){
+            return;
+        }
+
+        getAgents();
+    }, [authorizedUser]);
 
     return (
         <React.Fragment>
@@ -16,7 +34,13 @@ function UserChat(props) {
                     <div
                         className="user-chat-conversation"
                         id="messages">
-                            <h1>{t('Start the Chat with GPT-3.5 Model')}</h1>
+                            {/* Replacing h1 title with a selector */}
+                            <label htmlFor="agentSelector">{t('Select an agent to chat with:')}</label>
+                            <select id="agentSelector" name="agentSelector">
+                                {agents && agents.map((agent, index) => 
+                                    <option key={index} value={agent.user_id}>{agent.first_name + ' ' + agent.last_name}</option>
+                                )}
+                            </select>
                     </div>
                 </div>
                 <ChatInput/>
@@ -28,8 +52,15 @@ function UserChat(props) {
 const mapStateToProps = (state) => {
     return { 
         messages: state.Chat.messages,
-        newChat: state.Chat.newChat
+        newChat: state.Chat.newChat,
+        authorizedUser: state.User.authorizedUser,
+        agents: state.Agents.agents,
     }
 };
 
-export default withRouter(connect(mapStateToProps)(UserChat));
+const mapDispatchToProps = {
+    getAgents,
+    setActiveAgent,
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserChat));
