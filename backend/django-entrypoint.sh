7b5b603cf525cade5306f607cbcdf7dd11d5cb27
egortaran <egortaran@mail.ru>
2023-10-09 18:38:44 +0300

    #209 Add packeges

--- backend/django-entrypoint.sh
+++ backend/django-entrypoint.sh
@@ -3,7 +3,6 @@
 sh ./wait-for-postgres.sh
 
 python manage.py migrate
-python manage.py collectstatic --noinput
 
 #gunicorn picasso.wsgi --bind 0.0.0.0:8000
 python manage.py runserver 0.0.0.0:8000
