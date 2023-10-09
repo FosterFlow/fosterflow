88c8b1c04c512d423ca3c8b35ead62d60a3d8629
egortaran <egortaran@mail.ru>
2023-10-09 18:10:06 +0300

    #209 Make changes to start Docker Backend with Postgres

---
+++ backend/django-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sh ./wait-for-postgres.sh
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+#gunicorn picasso.wsgi --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "NAME": os.environ.get("POSTGRES_DB", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
