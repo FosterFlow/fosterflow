37d8dad1b395204eddcb84f2d7d54013a0ab7f65
Nikita <bragin_na@soshace.com>
2023-10-31 19:17:34 +0300

    #223 Updated environment variables for connection to database

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -99,11 +99,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("POSTGRES_DB", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("SQL_DATABASE", "fosterflow"),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "postgres"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
--- backend/wait-for-postgres.sh
+++ backend/wait-for-postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $SQL_HOST -d $POSTGRES_DB -U $POSTGRES_USER; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h $SQL_HOST -d $SQL_DATABASE -U $POSTGRES_USER; do
   echo >&2 "$(date +%Y%m%dt%H%M%S) Postgres is unavailable - sleeping"
   sleep 1
 done
