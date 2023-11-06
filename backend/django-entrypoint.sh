9620ca489a84e85c3ca7415e27295935b3d819af
Nikita <bragin_na@soshace.com>
2023-11-06 20:07:43 +0300

    #224 Splitted Dockerfiles to 2 versions for development environment and production environment

--- backend/django-entrypoint.sh
+++ backend/django-entrypoint.sh
@@ -4,5 +4,4 @@ sh ./wait-for-postgres.sh
 
 python manage.py migrate
 
-#gunicorn picasso.wsgi --bind 0.0.0.0:8000
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver $SERVER_API_URL
