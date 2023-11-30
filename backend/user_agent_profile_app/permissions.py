a7865f5f8dab7ef3b857bcad8e7abdab76410815
Nikita <bragin_na@soshace.com>
2023-11-30 22:17:15 +0300

    #245 Updated permissions for APIs: PATCH single self 'api/user_agent_profiles/{id}/ GET single 'api/user_agent_profiles_by_agent_id/{agent_id}/'

--- backend/user_agent_profile_app/permissions.py
+++ backend/user_agent_profile_app/permissions.py
@@ -1,22 +1,44 @@
 from rest_framework import permissions
 from agent_app.models import Agent
+from user_agent_profile_app.models import UserAgentProfile
 
-class IsOwnerAgent(permissions.BasePermission):
+class IsOwnerOfAgentProfile(permissions.BasePermission):
     message = {"errors": {"details": ["Available only for the owner"]}}
 
     def has_permission(self, request, view):
         # Get the user from the request
-        user = request.user
-
-        # Ensure the user is authenticated
-        if not user.is_authenticated:
+        authenticated_user = request.user
+        if not authenticated_user.is_authenticated:
             return False
 
-        # Fetch the agent associated with the user
-        # Assuming a direct ForeignKey relationship from Agent to User
         try:
-            agent = Agent.objects.get(user=user)
-            return True
+            authenticated_user_agent = Agent.objects.get(user=authenticated_user)
+        #TODO: return message if the obeject docent exists
         except Agent.DoesNotExist:
-            # Return False if no agent is associated with the user
+            return False
+        
+        profile_id = view.kwargs.get('pk')
+        try:
+            user_agent_profile = UserAgentProfile.objects.get(pk=profile_id)
+            return user_agent_profile.user_agent == authenticated_user_agent
+        #TODO: return message if the obeject docent exists
+        except UserAgentProfile.DoesNotExist:
+            return False
+        
+class IsOwnerOfAgent(permissions.BasePermission):
+    message = {"errors": {"details": ["Available only for the owner"]}}
+
+    def has_permission(self, request, view):
+        # Get the user from the request
+        authenticated_user = request.user
+        if not authenticated_user.is_authenticated:
+            return False
+        
+        agent_id = view.kwargs.get('agent_id')
+
+        try:
+            requested_user_agent = Agent.objects.get(pk=agent_id)
+            return requested_user_agent.user == authenticated_user
+        #TODO: return message if the obeject docent exists
+        except Agent.DoesNotExist:
             return False
--- backend/user_agent_profile_app/views.py
+++ backend/user_agent_profile_app/views.py
@@ -2,15 +2,13 @@ from rest_framework.generics import RetrieveAPIView, UpdateAPIView
 from .models import UserAgentProfile
 from .serializers import UserAgentProfileSerializer, UserAgentProfileAvatarSerializer
 from rest_framework.permissions import IsAuthenticated
-from .permissions import IsOwnerAgent
-from rest_framework import status
-from rest_framework.response import Response
+from .permissions import IsOwnerOfAgentProfile, IsOwnerOfAgent
 
 # Retrieve user agent profile by agent_id
 class UserAgentProfileByAgentIdView(RetrieveAPIView):
     queryset = UserAgentProfile.objects.all()
     serializer_class = UserAgentProfileSerializer
-    permission_classes = [IsAuthenticated, IsOwnerAgent]
+    permission_classes = [IsAuthenticated, IsOwnerOfAgent]
     lookup_field = 'user_agent_id'  # Adjust if the field name is different
     lookup_url_kwarg = 'agent_id'   # The name of the URL parameter
         
@@ -19,11 +17,11 @@ class UserAgentProfileDetailsView(UpdateAPIView):
     http_method_names = ['patch']
     queryset = UserAgentProfile.objects.all()
     serializer_class = UserAgentProfileSerializer
-    permission_classes = [IsAuthenticated, IsOwnerAgent]
+    permission_classes = [IsAuthenticated, IsOwnerOfAgentProfile]
 
 # Update avatar for user agent profile
 class UserAgentProfileAvatarView(UpdateAPIView):
     http_method_names = ['patch']
     queryset = UserAgentProfile.objects.all()
     serializer_class = UserAgentProfileAvatarSerializer
-    permission_classes = [IsAuthenticated, IsOwnerAgent]
+    permission_classes = [IsAuthenticated, IsOwnerOfAgentProfile]
