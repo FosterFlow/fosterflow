3ffc4ae66e18b4f05ec399957aa2be86eec90464
Nikita <bragin_na@soshace.com>
2023-12-08 22:42:42 +0300

    #248 Updated project middleware. Added "user_agent" to the request

--- backend/project/asgi.py
+++ backend/project/asgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
--- backend/project/middleware.py
+++ backend/project/middleware.py
@@ -1,4 +1,3 @@
-from django.utils.deprecation import MiddlewareMixin
 from agent_app.models import Agent
 from urllib.parse import parse_qs
 from channels.db import database_sync_to_async
@@ -7,13 +6,12 @@ from jwt import decode as jwt_decode
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-Users = get_user_model()
-
 @database_sync_to_async
 def get_user(token):
     try:
         decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-        user = get_user_model().objects.get(id=decoded_data['user_id'])
+        Users = get_user_model()
+        user = Users.objects.get(id=decoded_data['user_id'])
         return user
     except Exception as e:
         return AnonymousUser()
@@ -24,21 +22,30 @@ class TokenAuthMiddleWare:
         self.app = app
 
     async def __call__(self, scope, receive, send):
+        scope["user_agent"] = None
         try:
             token = parse_qs(scope["query_string"].decode("utf8"))["access"][0]
             user = await get_user(token)
             scope["user"] = user
+            scope["user_agent"] = Agent.objects.get(user=user)
             return await self.app(scope, receive, send)
         except Exception as e:
             await send({"type": "websocket.close"})
 
-class CurrentAgentMiddleware(MiddlewareMixin):
-    def process_request(self, request):
+class CurrentAgentMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         request.user_agent = None
-        print(f"CurrentAgentMiddleware request.user_agent {request} request.user {request.user}")
-        if request.user.is_authenticated:
-            try:
-                print(f"Agent.objects.get(user=request.user) {Agent.objects.get(user=request.user)}")
-                request.user_agent = Agent.objects.get(user=request.user)[0]
-            except Agent.DoesNotExist:
-                pass
+
+        try:
+            user = request.user
+            if user.is_authenticated:
+                request.user_agent = Agent.objects.get(user=user)
+        except Exception as e:
+            print(f"CurrentAgentMiddleware Exception {e}")
+            pass
+        
+        response = self.get_response(request)
+        return response
