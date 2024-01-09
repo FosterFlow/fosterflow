8430a26a0333a7b08eff03c712337ad4b61ffcd7
Nikita <bragin_na@soshace.com>
2024-01-09 23:52:20 +0300

    #258 Added Agent title to the Chat's header

--- frontend/src/pages/dashboard/UserChat/UserHead.js
+++ frontend/src/pages/dashboard/UserChat/UserHead.js
@@ -11,24 +11,34 @@ import { deleteChat as actionDeleteChat} from "../../../redux/chat/actions";
 function UserHead(props) {
     const location = useLocation();
     const isNewChat = location.pathname.startsWith('/chats/new_chat');
+    const {
+        activeChatId,
+        agents,
+        activeChatData,
+        actionDeleteChat,
+        router
+    } = props;
 
     function deleteChat(event) {
         event.preventDefault();
-        props.actionDeleteChat(props.activeChatId);
-        props.router.navigate("/chats/");
+        actionDeleteChat(activeChatId);
+        router.navigate("/chats/");
     }
 
     return (
         <React.Fragment>
             <div className="container-fluid user-chat-header">
                 <Row className="m-0">
-                    <Col sm={11} xs={10}>
-                        <Link to="/chats" className="user-chat-back d-lg-none text-muted p-2">
+                    <Col sm={1} xs={2} >
+                        <Link to="/chats" className="user-chat-back text-muted p-2 d-lg-none">
                             <i className="ri-arrow-left-s-line"></i>
                         </Link>
+                        {!isNewChat && (
+                            <span className="user-chat-agent pt-2 ps-2">Agent: {agents[activeChatData?.addressee_agent_id]}</span>
+                        )}
                     </Col>
                     {/* TODO: don't show on intial "/chats" page */}
-                    <Col sm={1} xs={2} className="text-end">
+                    <Col className="text-end">
                         {!isNewChat && (
                             <a href="#" onClick={(event) => deleteChat(event)} className="user-chat-delete p-2 pe-4 ri-delete-bin-line"></a>
                         )}
@@ -46,6 +56,8 @@ const mapStateToProps = (state) => {
     console.log("Dashabord Tabs UserHead mapStateToProps state", state);
     return { 
         activeChatId: state.Chat.activeChatId,
+        agents: state.Agents.agents,
+        activeChatData: state.Chat.activeChatData
     };
 };
 
