3b02140643cc72fcc58e59feefc5104f1b14733b
Nikita <bragin_na@soshace.com>
2024-01-15 18:12:28 +0300

    #261 Updated saga API for user_agent_profile API

--- frontend/src/redux/userAgentProfile/saga.js
+++ frontend/src/redux/userAgentProfile/saga.js
@@ -1,57 +1,51 @@
-import { 
-    call, 
-    put, 
-    takeEvery, 
-    delay 
-} from 'redux-saga/effects';
+import { call, put, takeEvery, delay } from 'redux-saga/effects';
 import apiAuthorizedClient from '../../helpers/apiAuthorizedClient';
 import {
     SET_ACTIVE_USER_AGENT_PROFILE,
-    GET_USER_AGENT_PROFILE, 
-    UPDATE_USER_AGENT_PROFILE_DATA, 
-    UPDATE_USER_AGENT_PROFILE_AVATAR 
+    GET_USER_AGENT_PROFILE,
+    UPDATE_USER_AGENT_PROFILE_DATA,
+    UPDATE_USER_AGENT_PROFILE_AVATAR
 } from './constants';
 import {
     setActiveUserAgentProfileSuccess,
     setActiveUserAgentProfileFailed,
-
     getUserAgentProfileSuccess,
     getUserAgentProfileFailed,
-
     updateUserAgentProfileDataInitState,
     updateUserAgentProfileDataSuccess,
     updateUserAgentProfileDataFailed,
-    
     updateUserAgentProfileAvatarInitState,
     updateUserAgentProfileAvatarSuccess,
     updateUserAgentProfileAvatarFailed
- } from './actions';
+} from './actions';
+
 const api = apiAuthorizedClient;
 
 function* setActiveUserAgentProfileSaga(action) {
     const userAgentProfileId = action.payload;
-
     try {
         const response = yield call(api.get, `/user-agent-profiles/${userAgentProfileId}/`);
-        yield put(setActiveUserAgentProfileSuccess(response));
+        yield put(setActiveUserAgentProfileSuccess(response.data));
     } catch (error) {
         yield put(setActiveUserAgentProfileFailed(error));
     }
 }
 
-function* getUserAgentProfileSaga({ payload: { id } }) {
+function* getUserAgentProfileSaga(action) {
+    const { id } = action.payload;
     try {
         const response = yield call(api.get, `/user-agent-profiles/${id}/`);
-        yield put(getUserAgentProfileSuccess(response));
+        yield put(getUserAgentProfileSuccess(response.data));
     } catch (error) {
         yield put(getUserAgentProfileFailed(error));
     }
 }
 
-function* updateUserAgentProfileDataSaga({ payload: { id, data } }) {
+function* updateUserAgentProfileDataSaga(action) {
+    const { id, data } = action.payload;
     try {
         const response = yield call(api.patch, `/user-agent-profiles/${id}/`, data);
-        yield put(updateUserAgentProfileDataSuccess(response));
+        yield put(updateUserAgentProfileDataSuccess(response.data));
         yield delay(10000);
         yield put(updateUserAgentProfileDataInitState());
     } catch (errors) {
@@ -59,18 +53,18 @@ function* updateUserAgentProfileDataSaga({ payload: { id, data } }) {
     }
 }
 
-function* updateUserAgentProfileAvatarSaga({ payload: { id, avatar } }) {
+function* updateUserAgentProfileAvatarSaga(action) {
+    const { id, avatar } = action.payload;
     try {
         let avatarData = new FormData();
-        
         avatarData.append('avatar', avatar);
-        const response = yield call(api.patch, `/user-agent-profiles/${id}/`, avatarData, {
+        const response = yield call(api.patch, `/user-agent-profiles/${id}/avatar/`, avatarData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             },
         });
 
-        yield put(updateUserAgentProfileAvatarSuccess(response.avatar));
+        yield put(updateUserAgentProfileAvatarSuccess(response.data.avatar));
         yield delay(10000);
         yield put(updateUserAgentProfileAvatarInitState());
     } catch (error) {
