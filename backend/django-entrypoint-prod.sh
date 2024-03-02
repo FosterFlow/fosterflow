eb1ab63053fc248b290fd44114fb51e80ef79704
Nikita <bragin_na@soshace.com>
2024-03-02 23:56:36 +0300

    #295 Yodated Docker production version for frontend

--- backend/django-entrypoint-prod.sh
+++ backend/django-entrypoint-prod.sh
@@ -5,4 +5,3 @@ sh ./wait-for-postgres.sh
 python manage.py migrate
 
 python manage.py runserver $SERVER_API_URL
-
--- frontend/install.sh
+++
@@ -1,2 +0,0 @@
-# TODO for production mode we have to switch yarn build, for development modeyarn start
-if [ ${PRODUCTION} ]; then 
