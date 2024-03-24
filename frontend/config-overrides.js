85592ab0a4d287d861459153d49d7557e41d6526
Nikita <bragin_na@soshace.com>
2024-03-24 22:30:22 +0300

    #296 Switch on script minimization

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -4,7 +4,7 @@ module.exports = function override(config, env) {
     // Do stuff with the Webpack config...
     if (env.PRODUCTION === "True") {
       // Disable minification in production by modifying the config:
-      config.optimization.minimize = false;
+      config.optimization.minimize = true;
     }
     return config;
   };
